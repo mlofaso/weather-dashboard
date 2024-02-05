205edee22443faf3f91b6dd06cd3a67267f01e39
Martin LoFaso <marty@Martins-Air.attlocal.net>
2024-02-04 18:31:21 -0800

    HTML & JS edits

--- Assets copy/js/script.js
+++ Assets copy/js/script.js
@@ -8,8 +8,9 @@
 // THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
 // TODO: Use fetch to link to 5 day forecast and geocoding apis, add eventlistener for click on submit button that will trigger the function to return the searched city's weather
 
-var currentCity = document.getElementById("current-city");
+var currentCity = document.getElementById("currentCity");
 var searchBtn = document.getElementById("searchBtn");
+var cityName = document.getElementById("cityName");
 
 function getApi() {
   var requestUrl =
@@ -30,3 +31,5 @@ console.log("search");
 // WHEN I search for a city, that city is added to the search history
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
+
+// Geocoding API: http://api.openweathermap.org/geo/1.0/direct?q=&limit=5&appid={0e0b7c450f9c8931ca8495ab3a500c20}
