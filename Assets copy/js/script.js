c9a4f163cea611b3973dc13f1a6248c68f6239e1
Martin LoFaso <marty@Martins-Air.attlocal.net>
2024-02-04 18:07:28 -0800

    JS edits

--- Assets copy/js/script.js
+++ Assets copy/js/script.js
@@ -9,11 +9,23 @@
 // TODO: Use fetch to link to 5 day forecast and geocoding apis, add eventlistener for click on submit button that will trigger the function to return the searched city's weather
 
 var currentCity = document.getElementById("current-city");
+var searchBtn = document.getElementById("searchBtn");
 
 function getApi() {
   var requestUrl =
-    "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
+    "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={0e0b7c450f9c8931ca8495ab3a500c20}";
+
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
 }
+
+searchBtn.addEventListener("click", getApi);
+console.log("search");
 
 // WHEN I search for a city, that city is added to the search history
 // WHEN I click on a city in the search history
