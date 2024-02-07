75b9848d5758e83650451b16363653851eec0fea
Martin LoFaso <marty@Martins-Air.attlocal.net>
2024-02-06 18:17:09 -0800

    JS & CSS edits

--- Assets copy/js/script.js
+++ Assets copy/js/script.js
@@ -1,12 +1,11 @@
 // GIVEN a weather dashboard with form inputs
 
 // WHEN I search for a city
-// THEN I am presented with current and future conditions for that city
+// THEN I am presented with future conditions for that city
 // WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
+// THEN I am presented with the date, an icon representation of weather conditions, the humidity, and the wind speed
 // WHEN I view future weather conditions for that city
 // THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-// TODO: Use fetch to link to 5 day forecast and geocoding apis, add eventlistener for click on submit button that will trigger the function to return the searched city's weather
 
 var currentCity = document.getElementById("currentCity");
 var searchBtn = document.getElementById("searchBtn");
@@ -14,7 +13,6 @@ var userInput = document.getElementById("cityName");
 var forecast = document.getElementById("forecast");
 
 function getApiWeather(x) {
-  // grab the value for the city name and insert it into the apiurl
   console.log(x);
   var requestUrl =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
@@ -31,7 +29,6 @@ function getApiWeather(x) {
 }
 
 function getApiForecast(x) {
-  // grab the value for the city name and insert it into the apiurl
   console.log(x);
   var requestUrl =
     "https://api.openweathermap.org/data/2.5/forecast?q=" +
@@ -49,24 +46,34 @@ function getApiForecast(x) {
 
 function captureData(event) {
   event.preventDefault();
-  // this function needs to grab the value of the search field
-  // then pass it into Getapi function
   getApiWeather(userInput.value);
   getApiForecast(userInput.value);
 }
 
 function displayCurrent(data) {
   currentCity.innerHTML = "";
-  console.log(data);
   var currentCityEl = document.createElement("h2");
   currentCityEl.textContent = data.name;
   var tempEl = document.createElement("p");
-  tempEl.textContent = "Temp: " + data.main.temp;
+  tempEl.textContent = "Temp: " + data.main.temp + "°F";
   var windEl = document.createElement("p");
-  windEl.textContent = "Wind: ";
+  windEl.textContent = "Wind: " + data.wind.speed + " MPH";
   var humidityEl = document.createElement("p");
-  humidityEl.textContent = "Humidity: ";
+  humidityEl.textContent = "Humidity: " + data.main.humidity + "%";
   currentCity.append(currentCityEl, tempEl, windEl, humidityEl);
+}
+
+function displayForecast(data) {
+  forecast.innerHTML = "";
+  var El = document.createElement("h3");
+  forecastEl.textContent = data.;
+  var El = document.createElement("");
+  El.textContent = "" + data. + "";
+  var El = document.createElement("");
+  El.textContent = "" + data. + "";
+  var El = document.createElement("");
+  El.textContent = "Humidity: " + data.main.humidity + "%";
+  forecast.append(El, El, El, El);
 }
 
 searchBtn.addEventListener("click", captureData);
@@ -74,5 +81,3 @@ searchBtn.addEventListener("click", captureData);
 // WHEN I search for a city, that city is added to the search history
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
-
-// Geocoding API: http://api.openweathermap.org/geo/1.0/direct?q=&limit=5&appid={0e0b7c450f9c8931ca8495ab3a500c20}
